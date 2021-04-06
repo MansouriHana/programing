e9cfb49218eb3c501bb9e02dd6f2903fa5d38f21
MansouriHana <mansouriii.hanaa@gmail.com>
2021-04-06 21:26:32 +0100

    form group task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-group-app';
+  userForm;
+constructor(private fb:FormBuilder){
+this.userForm= this.fb.group({
+  name:[null, Validators.required],
+  password:[null, [Validators.required, Validators.min(6)]]
+});
+}
+
+  onSubmit(){
+    console.log(this.userForm);
+  }
 }
